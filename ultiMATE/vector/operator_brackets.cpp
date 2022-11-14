14962d19b200838edec74bdd8d0be0cfd80c3b18
Maxence Liboz <mliboz@z1r3p5.42lyon.fr>
2022-11-14 14:14:50 +0100

    Fix (Operator brackets) Change name of the function that throw a compile error

--- ultiMATE/vector/operator_brackets.cpp
+++ ultiMATE/vector/operator_brackets.cpp
@@ -3,27 +3,27 @@
 std::string testName("operator_brackets");
 
 template <typename T>
-void test(std::ofstream& ofs, vector<T>& vector1);
+void test_on_vector(std::ofstream& ofs, vector<T>& vector1);
 
 int main(int argc, char** argv)
 {
 	vector<int> v1(10, 42);
 	v1[3] = 420;
-	test<int>(ofs, v1);
+	test_on_vector<int>(ofs, v1);
 
 	vector<double> v2(1000, 42.424242f);
 	v2[500] = 0.0f;
-	test<double>(ofs, v2);
+	test_on_vector<double>(ofs, v2);
 
 	vector<TestStruct> v3(100, TestStruct(10, 42, "test"));
 	v3[500] = TestStruct(-5, 213, "nothing");
-	test<TestStruct>(ofs, v3);
+	test_on_vector<TestStruct>(ofs, v3);
 
 	ofs.close();
 }
 
 template <typename T>
-void test(std::ofstream& ofs, vector<T>& vector1)
+void test_on_vector(std::ofstream& ofs, vector<T>& vector1)
 {
 	TEST_INIT();
 
