1b613d9035966fb7324336f7034c3363d86c9da0
Cyril Battistolo <cybattis@student.42lyon.fr>
2022-11-15 15:18:46 +0100

    test(max_size): fix max_size tests.
    print the max_size value this time...

--- ultiMATE/vector/max_size.cpp
+++ ultiMATE/vector/max_size.cpp
@@ -10,33 +10,38 @@ int main(int argc, char** argv)
 {
 	{ // max_size vector<float>
 		TEST_INIT();
+		vector<char> v;
+		write_result(ofs, v.max_size());
+	}
+	{ // max_size vector<float>
+		TEST_INIT();
+		vector<unsigned char> v;
+		write_result(ofs, v.max_size());
+	}
+	{ // max_size vector<float>
+		TEST_INIT();
 		vector<int> v;
-		v.max_size();
-		write_result(ofs, v);
+		write_result(ofs, v.max_size());
 	}
 	{ // max_size vector<float>
 		TEST_INIT();
 		vector<long> v(50);
-		v.max_size();
-		write_result(ofs, v);
+		write_result(ofs, v.max_size());
 	}
 	{ // max_size vector<float>
 		TEST_INIT();
 		vector<float> v(5, 42);
-		v.max_size();
-		write_result(ofs, v);
+		write_result(ofs, v.max_size());
 	}
 	{ // max_size vector<std::string>
 		TEST_INIT();
 		vector<std::string> v(50, "heyyyyyy!");
-		v.max_size();
-		write_result(ofs, v);
+		write_result(ofs, v.max_size());
 	}
 	{ // max_size vector<TestStruct>
 		TEST_INIT();
 		vector<TestStruct> v(5, TestStruct(5, -42, "test"));
-		v.max_size();
-		write_result(ofs, v);
+		write_result(ofs, v.max_size());
 	}
 
 	ofs.close();
