24080b06f4196c457ee69bf82de41559fec25a3a
Maxence Liboz <mliboz@z1r3p4.42lyon.fr>
2022-11-17 11:31:30 +0100

    Test(Copy_constructor): Add tests to check the capacity after copy

--- ultiMATE/vector/copy_constructor.cpp
+++ ultiMATE/vector/copy_constructor.cpp
@@ -5,6 +5,9 @@ std::string testName("copy_constructor");
 template <typename T>
 void test_for_type(const vector<T>& vector1);
 
+template <typename T>
+void test_for_capacity(const vector<T> vector1);
+
 int main(int argc, char** argv)
 {
 	vector<int> v1(10, 42);
@@ -19,6 +22,21 @@ int main(int argc, char** argv)
 	v3[50] = TestStruct(-5, 213, "nothing");
 	test_for_type<TestStruct>(v3);
 
+	v1.clear();
+	for(int i = 0; i < 12; i++)
+		v1.push_back(i);
+	test_for_capacity<int>(v1);
+
+	v2.clear();
+	for(double i = 0; i < 10; i++)
+		v2.push_back(i);
+	test_for_capacity<double>(v2);
+
+	v3.clear();
+	for(double i = 0; i < 10; i++)
+		v3.push_back(TestStruct(i, i * 10, "nothing"));
+	test_for_capacity<TestStruct>(v3);
+
 	ofs.close();
 }
 
@@ -30,3 +48,10 @@ void test_for_type(const vector<T>& vector1)
 	write_result<T>(ofs, vector2);
 }
 
+template <typename T>
+void test_for_capacity(const vector<T> vector1)
+{
+	TEST_INIT();
+	vector<T> vector2(vector1);
+	write_result<T>(ofs, vector2);
+}
