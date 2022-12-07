172c99a94e1c746c3a100ecf0adaa2d23e17e87f
B-Bischoff <mainbischoffbrice@gmail.com>
2022-12-07 10:58:05 +0100

    functionning tests

--- map_common.cpp
+++ map_common.cpp
@@ -2,11 +2,10 @@
 
 namespace std {
 
-	template <class U, class T>
-	std::string to_string(const CURRENT_NAMESPACE::map<U, T>& m) {
+	template <class T, class U>
+	std::string to_string(const CURRENT_NAMESPACE::map<T, U>& m) {
 		std::string str;
-		//typename CURRENT_NAMESPACE::map<U, T>::const_iterator it;
-		typename CURRENT_NAMESPACE::map<U, T>::iterator it;
+		typename CURRENT_NAMESPACE::map<T, U>::const_iterator it;
 		for (it = m.begin(); it != m.end(); ++it)
 		{
 			str += std::to_string(it->first);
@@ -16,12 +15,12 @@ namespace std {
 		}
 		return str;
 	}
-};
+}
 
-template <class U, class T>
-void write_result(std::ofstream& ofs, const CURRENT_NAMESPACE::map<U, T>& map, const bool printContent=true) {
-	//ofs << "size: " + std::to_string(map.size()) << std::endl;
+template <class T, class U>
+void write_result(std::ofstream& ofs, const CURRENT_NAMESPACE::map<T, U>& map, const bool printContent=true) {
+	ofs << "size: " + std::to_string(map.size()) << std::endl;
 	ofs << "empty: " + std::to_string(map.empty()) << std::endl;
-	//if (printContent)
-	//	ofs << std::to_string(map);
+	if (printContent)
+		ofs << std::to_string(map);
 }
