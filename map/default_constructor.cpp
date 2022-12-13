0a9eaed10d3b097871713b10840ebe04fa11ba52
Archimbaud <tharchimb@gmail.com>
2022-12-13 13:07:23 +0100

    Feat (constructors)Add range_constructor copy_constructor test, added Struct to comp Template

--- map/default_constructor.cpp
+++ map/default_constructor.cpp
@@ -2,7 +2,7 @@
 
 std::string testName = "default_constructor";
 
-template <class T, class U>
+template <class T, class U, class C = std::less<T> >
 void test_for_type();
 
 int main(void)
@@ -12,14 +12,19 @@ int main(void)
 	test_for_type<std::string, int>();
 	test_for_type<TestStruct, double>();
 	test_for_type<TestStruct, TestStruct>();
+    test_for_type<int, int, ft_greater<int> >();
+    test_for_type<double, char, ft_greater<double> >();
+    test_for_type<std::string, int, ft_greater<std::string> >();
+    test_for_type<TestStruct, TestStruct, ft_greater<TestStruct> >();
+    test_for_type<TestStruct, double, ft_greater<TestStruct> >();
 
-	ofs.close();
+    ofs.close();
 }
 
-template <class T, class U>
+template <class T, class U, class C>
 void test_for_type()
 {
 	TEST_INIT();
-	CURRENT_NAMESPACE::map<T, U> m;
+	CURRENT_NAMESPACE::map<T, U, C> m;
 	write_result(ofs, m, false);
 }
